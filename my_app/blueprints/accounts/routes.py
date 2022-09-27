91f07bcc5f01ef797da64f690aca4951c6331717
Nguyen Duong <minhhuy1411417@gmail.com>
2022-09-27 03:42:06 -0700

    Transfer route

--- my_app/blueprints/accounts/routes.py
+++ my_app/blueprints/accounts/routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
-from .resources import BankAccount, Deposit
+from .resources import BankAccount, Deposit, Transaction, Transfer
 
 accounts = Blueprint('accounts', __name__, url_prefix='/accounts')
 
 api = Api(accounts)
 api.add_resource(BankAccount, '/')
 api.add_resource(Deposit, '/deposit')
+api.add_resource(Transaction, '/<uuid:account_id>/transaction')
+api.add_resource(Transfer, '/<uuid:account_id>/transfer')
